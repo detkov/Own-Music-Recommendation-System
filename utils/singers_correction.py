a689323d5b1fba4a06915238f7a24c4a4cf5dd70
Nikita <nds-gfk@mail.ru>
2019-07-11 01:31:09 +0500

    A lot of little improvements also parser is almost ready (but is hidden)

--- utils/singers_correction.py
+++ utils/singers_correction.py
@@ -1,5 +1,17 @@
-def correct_singers_file(name='singers.txt'):
-    singers = list(set(open(name).read().split('\n')))
+import sys
+
+def correct_singers_file(filename='singers.txt'):
+    # filename = '../' + filename
+    singers = list(set(open(filename).read().split('\n')))
     singers.sort()
-    with open (name, 'w') as f:
+    with open (filename, 'w') as f:
         f.write('\n'.join(singers))
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        correct_singers_file()
+        pass
+    else:
+        filename = sys.argv[1]
+        correct_singers_file(filename=filename)
+        pass
